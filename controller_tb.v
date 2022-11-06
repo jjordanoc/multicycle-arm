e4286710d981843609a53748b51bdac2690998be
Jose Chachi <jose.chachi@utec.edu.pe>
2022-11-05 22:55:57 -0500

    <feat>: add testvector.

--- controller_tb.v
+++ controller_tb.v
@@ -1,7 +1,7 @@
 `timescale 1ns/1ps
 module controller_tb;
     reg clk, reset;
-    reg [101:0] testvector[63:0];
+    reg [35:0] testvector[35:0];
     reg [31:0] vectornum;
     
     reg [31:0] errors;
@@ -11,6 +11,11 @@ module controller_tb;
     wire [31:0] Result;
     reg [31:0] Expected;
     
+    reg flag;
+    reg Op;
+    reg Funct;
+
+    integer i = 0;
 
     wire PCWrite;
 	wire MemWrite;
@@ -24,7 +29,7 @@ module controller_tb;
 	wire [1:0] ImmSrc;
 	wire [1:0] ALUControl;
 
-    controller dut(.clk(clk), .reset(reset), .Instr(Instr), .ALUFlags(ALUFlags), .PCWrite(PCWrite), .MemWrite(MemWrite), .RegWrite(RegWrite), .IRWrite(IRWrite), .AdrSrc(AdrSrc), .RegSrc(RegSrc), .ALUSrcA(ALUSrcA), .ALUSrcB(ALUSrcB), .ResultSrc(ResultSrc), .ImmSrc(ImmSrc), .ALUControl(ALUControl))
+    controller dut(.clk(clk), .reset(reset), .Instr(Instr[31:12]), .ALUFlags(ALUFlags), .PCWrite(PCWrite), .MemWrite(MemWrite), .RegWrite(RegWrite), .IRWrite(IRWrite), .AdrSrc(AdrSrc), .RegSrc(RegSrc), .ALUSrcA(ALUSrcA), .ALUSrcB(ALUSrcB), .ResultSrc(ResultSrc), .ImmSrc(ImmSrc), .ALUControl(ALUControl));
 
     always begin
       clk=1; #5; clk=0; #5;    
@@ -34,31 +39,43 @@ module controller_tb;
       $readmemh("controller_tv.tv", testvector);      // readmemb("filename",dest):  read file b is binary formay, h is hexadecimal format
       errors=0;
       vectornum=0;
-      reset = 1; #2; reset=0;
+      reset <= 1; #2; reset<=0;
     end
 
     always @(posedge clk) begin
-        Instr = testvector[vectornum][31:0];
-        ALUFlags = testvector[vectornum][32+4:32];
-        Expected = testvector[vectornum][32+4+1+12:32+4+1];
-    end
 
-    always @(negedge clk) begin
-        if  (~reset) begin
-            if (Result !== Expected) begin
-                $display("testvector: %h",testvector[vectornum]);
-                $display("Vectornum: %d",vectornum);
-                $display("instr: %d", Instr);
-                $display("output Result:%b, expected Result:%b",Result,Expected);
-                errors=errors+1;
-            end
+        if (i === 0) begin
+            i = i + 1;
+            Instr = testvector[vectornum][31+4:4];
+            ALUFlags = testvector[vectornum][3:0];
+            Op = Instr[27:26];
+            Funct = Instr[20];
             vectornum=vectornum+1;
-            if (testvector[vectornum][0] === 1'bx) begin
-                $display("total errors: %d",errors);
-                $finish;  
-            end
+        end
+        else if (i === 2 & Op === 2'b10) begin
+            i = 0;
+        end
+
+        else if (i === 3 & Op === 2'b01 & Funct === 0)begin
+            i = 0;
+        end
+        else if (i === 3 & Op === 2'b00)begin
+            i = 0;
+        end
+        else if (i === 4 & Op === 2'b01 & Funct === 1)begin
+            i = 0;
+        end
+        
+        else begin
+        i = i + 1;
+        end
+
+        if (testvector[vectornum][0] === 1'bx) begin
+            $finish;
         end
     end
+
+
 
     initial begin
           $dumpfile("controller.vcd");
